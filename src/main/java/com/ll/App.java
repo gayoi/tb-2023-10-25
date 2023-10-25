f63237b77c1c021a69caa487633447784d3bbe39
gayoi <dbs9476@naver.com>
2023-10-25 22:37:15 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,6 +1,19 @@
 package com.ll;
+
+import java.util.Scanner;
+
 class App {
-    void run(){
-        System.out.println("프로그램 실행");
+    void run() {
+        System.out.println("=== 명언 앱 ===");
+
+        System.out.print("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine(); //한줄이 입력될때까지 멈춘다
+
+        System.out.printf("입력하신 명령 : %s\n", cmd);
+        // 위 라인은 아래 라인과 같은 뜻 cmd안에 값이 %s로 치환이 되서 문장이 완성
+        //System.out.printf("입력하신 명령 : %s\n");
+
     }
 }
