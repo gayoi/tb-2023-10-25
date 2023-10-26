1b45bc98764a39b54f717e027b88998320b3264d
gayoi <dbs9476@naver.com>
2023-10-26 10:49:03 +0900

    3단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,7 +7,7 @@ class App {
         while (true){
             System.out.println("=== 명언 앱 ===");
 
-            System.out.print("명령) ");
+            System.out.print("명언 ) ");
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine(); //한줄이 입력될때까지 멈춘다
@@ -16,8 +16,13 @@ class App {
                 break; // cotinue 이번 텀만 쉬는것 break 반복문 자체가 끝나는것!!
             }
             else if(cmd.equals("등록")) {
-                System.out.println("명언 : ");
-                System.out.println("작가 : ");
+                System.out.print("명언 : ");
+                String content = scanner.nextLine(); // 입력을 기다린다
+
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다");
             }
             System.out.printf("입력하신 명령 : %s\n", cmd);
             // 위 라인은 아래 라인과 같은 뜻 cmd안에 값이 %s로 치환이 되서 문장이 완성
