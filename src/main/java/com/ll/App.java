20fce1fe7836fceda90b786989df4c4cc0d41dfa
gayoi <dbs9476@naver.com>
2023-10-26 11:17:49 +0900

    step4, print no, out of loop block

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,33 +1,41 @@
 package com.ll;
-
 import java.util.Scanner;
 
-class App {
+public class App {
     void run() {
-        while (true){
-            System.out.println("=== 명언 앱 ===");
+        System.out.println("== 명언 앱 == ");
 
-            System.out.print("명언 ) ");
+        int lastQuotationId = 0; // 장기기억이라서..? count 변수
 
-            Scanner scanner = new Scanner(System.in);
-            String cmd = scanner.nextLine(); //한줄이 입력될때까지 멈춘다
+            while(true) {
 
-            if(cmd.equals("종료")) { //string 비교할떄 == 아니라 000.equals이다! 외워라
-                break; // cotinue 이번 텀만 쉬는것 break 반복문 자체가 끝나는것!!
+                System.out.print("명언 ) ");
+
+                Scanner scanner = new Scanner(System.in);
+                String cmd = scanner.nextLine(); //한줄이 입력될때까지 멈춘다
+
+                if(cmd.equals("종료")) { //string 비교할떄 == 아니라 000.equals이다! 외워라
+                    break; // cotinue 이번 텀만 쉬는것 break 반복문 자체가 끝나는것!!
+                }
+                else if(cmd.equals("등록")) {
+
+                    System.out.print("명언 : ");
+                    String content = scanner.nextLine(); // 입력을 기다린다
+
+                    System.out.print("작가 : ");
+                    String authorName = scanner.nextLine();
+
+                    lastQuotationId++;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
+                }
+                System.out.printf("입력하신 명령 : %s\n", cmd);
+                // 위 라인은 아래 라인과 같은 뜻 cmd안에 값이 %s로 치환이 되서 문장이 완성
+                //System.out.printf("입력하신 명령 : %s\n");
             }
-            else if(cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine(); // 입력을 기다린다
 
-                System.out.print("작가 : ");
-                String authorName = scanner.nextLine();
-
-                System.out.println("1번 명언이 등록되었습니다");
-            }
-            System.out.printf("입력하신 명령 : %s\n", cmd);
-            // 위 라인은 아래 라인과 같은 뜻 cmd안에 값이 %s로 치환이 되서 문장이 완성
-            //System.out.printf("입력하신 명령 : %s\n");
         }
 
+
     }
-}
+
